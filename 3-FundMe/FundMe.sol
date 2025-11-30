d53eb0c5aa035b26816c7d5199f8d2cc48dba0ef
gianellacoronel <gia.ne12@hotmail.com>
2025-11-30 14:43:57 -0500

    feat: creating my own library

--- 3-FundMe/FundMe.sol
+++ 3-FundMe/FundMe.sol
@@ -5,10 +5,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-//importing interface from Chainlink documentation
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+// Importing my own library
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 5e18; // We change it because we are using getConversionRate in fund function.
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
@@ -20,7 +22,10 @@ contract FundMe {
         // 1. How do we send ETH to this contract?
 
         // msg.value is the amount you put on Deploy & run transactions tab
-        require(getConversionRate(msg.value) > minimumUsd, "didn't send enough ETH");
+        //require(getConversionRate(msg.value) > minimumUsd, "didn't send enough ETH");
+        
+        msg.value.getConversionRate(); //"msg.value is like the parameter for getConversionRate"
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enought ETH");
         funders.push(msg.sender); //It's the account that do the transaction
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 
@@ -30,29 +35,4 @@ contract FundMe {
     }
     
     function withdraw() public {}
-
-    function getPrice() public view returns(uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        
-        // () when function returns a lot of values
-        //(uint80 roundId, int256 price, uint256 startedAt, uint256 timestamp, uint80 answeredInRound) = priceFeed.latestRoundData();
-        // Like we only need "price", we can delete the rest, keeping the commas
-        (,int256 price,,,) = priceFeed.latestRoundData();
-
-        //Price of ETH in terms of USD
-        //Learning how to cast numbers
-        return uint256(price * 1e10);
-    }
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        //1 ETH?
-        uint256 ethPrice = getPrice(); //2000_000000000000000000
-        
-        // Always you want to multiply before divide
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; //(2000_000000000000000000 * 1_0000000000000000000) / 1e18;
-        
-        return ethAmountInUsd; //$2000 = 1ETH
-    }
-
 }
---
+++ 3-FundMe/PriceConverter.sol
@@ -0,0 +1,32 @@
+//library is similar tu a contract
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter {
+    function getPrice() internal view returns(uint256){
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        
+        // () when function returns a lot of values
+        //(uint80 roundId, int256 price, uint256 startedAt, uint256 timestamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+        // Like we only need "price", we can delete the rest, keeping the commas
+        (,int256 price,,,) = priceFeed.latestRoundData();
+
+        //Price of ETH in terms of USD
+        //Learning how to cast numbers
+        return uint256(price * 1e10);
+    }
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        //1 ETH?
+        uint256 ethPrice = getPrice(); //2000_000000000000000000
+        
+        // Always you want to multiply before divide
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; //(2000_000000000000000000 * 1_0000000000000000000) / 1e18;
+        
+        return ethAmountInUsd; //$2000 = 1ETH
+    }
+}
